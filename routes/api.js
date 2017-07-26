1348c4fba10978fbb108ee65a3cc11538d320985
Nathan Dane <nathan@nathandane.co.uk>
2017-07-26 18:00:01 +0100

    API cleanup

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,6 @@
 let express = require('express');
 let router = express.Router();
 
-/* Index Routes */
-// router.route('/')
-//       .get('/', function(req, res, next) {
-//         res.render('index');
-//       });
-
 /* User Routes */
 let UserController = require('../controllers/UserController');
 router.route('/users')
