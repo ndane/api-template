a779f0e370c8fd6c1fb5a5d5a0312721a3a34772
Nathan Dane <nathan@nathandane.co.uk>
2017-08-05 13:55:59 +0100

    Added password hashing

--- models/User.js
+++ models/User.js
@@ -1,15 +1,20 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltWorkFactor = 10;
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: 'Please enter a username',
+    index: { unique: true },
   },
   email: {
     type: String,
     required: 'Please enter an email address',
+    index: { unique: true },
   },
   password: {
     type: String,
@@ -20,5 +25,33 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// Password hashing middleware for mongoose
+/* eslint-disable consistent-return */
+UserSchema.pre('save', (next) => {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(saltWorkFactor, (err, salt) => {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Password matching
+UserSchema.methods.comparePassword = (password, cb) => {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) { return cb(err); }
+    cb(null, isMatch);
+  });
+};
+/* eslint-enable consistent-return */
 
 module.exports = mongoose.model('Users', UserSchema);
