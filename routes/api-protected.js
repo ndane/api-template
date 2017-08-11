3d2d7923e441e8ffc6461cbf8ccd7e13129024a5
Nathan Dane <nathan@nathandane.co.uk>
2017-08-11 14:14:30 +0100

    Added authentication middleware

--- routes/api-protected.js
+++ routes/api-protected.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import Secrets from '../config/secrets';
 
 /* Controllers */
 import UserController from '../controllers/UserController';
@@ -6,10 +8,34 @@ import UserController from '../controllers/UserController';
 /* Base Router */
 const router = express.Router();
 
+/* JWT Authentication */
+function respondWithError(res) {
+  return res.status(403).json({ message: 'Forbidden' });
+}
+
+router.use((req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers.bearer;
+
+  if (!token) {
+    return respondWithError(res);
+  }
+
+  // Check token
+  jwt.verify(token, Secrets.key, (err, decoded) => {
+    if (err) {
+      return respondWithError(res);
+    }
+
+    req.decoded = decoded;
+    return next();
+  });
+
+  return null;
+});
+
 /* User Routes */
 router.route('/users')
-  .get(UserController.users)
-  .post(UserController.create);
+  .get(UserController.users);
 
 router.route('/users/:id')
   .get(UserController.user)
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 /* Controllers */
 import AuthController from '../controllers/AuthController';
+import UserController from '../controllers/UserController';
 
 /* Base Router */
 const router = express.Router();
@@ -9,5 +10,9 @@ const router = express.Router();
 /* Auth Routes */
 router.route('/authenticate')
   .post(AuthController.authenticate);
+
+/* User Routes */
+router.route('/users')
+  .post(UserController.create);
 
 export default router;
